5b0219c077e6d3be46fd2dbdcfec20e0de0db0db
Connor Cooper <cooper.connor.17@gmail.com>
2024-06-28 10:03:14 +1000

    Root flake.nix

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   descriptions = "A personal collection of flake templates";
 
   outputs = { self }: {
-    python = {
-      path = ./python;
-      description = "Python template, using poetry2nix";
-      welcomeText = ''
-        # Getting started
-        - Run `direnv allow`
-      '';
+    templates = {
+      python = {
+        path = ./python;
+        description = "Python template, using poetry2nix";
+        welcomeText = ''
+          # Getting started
+          - Run `direnv allow`
+        '';
+      };
     };
   };
 }
